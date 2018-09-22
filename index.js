83f0a944a7e77aad289b899d4179a8760d5aeac5
Abby Zhang <abbyzhang@Abbys-MBP.home>
2018-09-22 12:30:58 -1000

    ✅  TEST: add on

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const bp = require('body-parser')
+const exphbs = require('express-handlebars')
+// const lifeguard = require('./lifeguard.js')
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('public'))
+app.use(bp.urlencoded({ extended: true }))
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+// app.use('/', lifeguard)
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(PORT, () => {
+    console.log(`Started app on port: ${PORT}`);
+})
